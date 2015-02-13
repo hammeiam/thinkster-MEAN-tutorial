e0b76e74663129fdbcddad9866aedfe279e306f5
David Hamme <dhamme@gmail.com>
2015-02-12 23:50:59 -0800

    add new post, validate title presence

--- app.js
+++ app.js
@@ -4,5 +4,16 @@ app.controller('MainCtrl', [
 	'$scope',
 	function($scope){
 		$scope.test = 'Hello world!';
-		$scope.posts = ['post1', 'post2', 'post3', 'post4'];
+		$scope.posts = [
+			{title: 'post 1', upvotes: 5},
+		  {title: 'post 2', upvotes: 2},
+		  {title: 'post 3', upvotes: 15},
+		  {title: 'post 4', upvotes: 9},
+		  {title: 'post 5', upvotes: 4}
+	  ];
+	  $scope.addPost = function(){
+	  	if(!$scope.title || $scope.title === '') { return; }
+	  	$scope.posts.push({title: $scope.title, upvotes: 0});
+	  	$scope.title = '';
+	  }
 	}]);
