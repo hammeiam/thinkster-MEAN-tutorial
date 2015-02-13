b8b9e7c1898f26806592b2576d5d1213a8d356f8
David Hamme <dhamme@gmail.com>
2015-02-12 23:53:25 -0800

    basic upvoting

--- app.js
+++ app.js
@@ -15,5 +15,8 @@ app.controller('MainCtrl', [
 	  	if(!$scope.title || $scope.title === '') { return; }
 	  	$scope.posts.push({title: $scope.title, upvotes: 0});
 	  	$scope.title = '';
-	  }
+	  };
+	  $scope.incrementUpvotes = function(post) {
+		  post.upvotes += 1;
+		};
 	}]);
