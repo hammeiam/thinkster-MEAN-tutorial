5d5621921a1745b7f33aed5ad9931e90d6606f89
David Hamme <dhamme@gmail.com>
2015-02-13 00:00:07 -0800

    bootstrap styles

--- app.js
+++ app.js
@@ -13,8 +13,13 @@ app.controller('MainCtrl', [
 	  ];
 	  $scope.addPost = function(){
 	  	if(!$scope.title || $scope.title === '') { return; }
-	  	$scope.posts.push({title: $scope.title, upvotes: 0});
+	  	$scope.posts.push({
+	  		title: $scope.title,
+	  		link: $scope.link,
+	  		upvotes: 0
+	  	});
 	  	$scope.title = '';
+	  	$scope.link = '';
 	  };
 	  $scope.incrementUpvotes = function(post) {
 		  post.upvotes += 1;
