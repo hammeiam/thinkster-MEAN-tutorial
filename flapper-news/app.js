1e474fdbcf6ef169ecd1c47774962867b2a0b971
David Hamme <dhamme@gmail.com>
2015-02-14 14:30:04 -0800

    defined Comments and Posts

--- flapper-news/app.js
+++ flapper-news/app.js
@@ -8,6 +8,11 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+var mongoose = require('mongoose');
+require('./models/Posts');
+require('./models/Comments');
+mongoose.connect('mongodb://localhost/news');
+
 var app = express();
 
 // view engine setup
